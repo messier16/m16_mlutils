f103d9415d274a5990e9bb666d0ce48304fb9e40
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-10-13 08:46:10 +0100

    Fixed Integration with pipeline Test

--- tests/test_IntegrationWithPipeline.py
+++ tests/test_IntegrationWithPipeline.py
@@ -1,32 +1,37 @@
-from .NumPyTestCase import NumPyTestCase
-from sklearn.pipeline import Pipeline
-import pandas as pd
 import numpy as np
-import os
+import pandas as pd
+from sklearn.pipeline import Pipeline
 
-from m16_mlutils.pipeline import DataFrameSelector, CategoryEncoder
+from m16_mlutils.pipeline import CategoryEncoder, DataFrameSelector
+from .NumPyTestCase import NumPyTestCase
+
 
 class test_IntegrationWithPipeline(NumPyTestCase):
 
     def test_pipeline(self):
-
-        train = pd.read_csv("./tests/fakedata/train.csv")
         test = pd.read_csv("./tests/fakedata/test.csv")
 
+        categorical_columns = [
+            "HouseStyle",
+            "Foundation",
+            "KitchenQual",
+            "ExterQual",
+            "ExterCond",
+            'Exterior1st',
+            "LotShape"
+        ]
+
+        unique_values = 0
+        # Count the number of different values as that is
+        # the number of columns in the transformed data frame
+        for column in categorical_columns:
+            unique_values += len(test[column].unique())
 
         categorical_pipeline = Pipeline([
-            ("selector", DataFrameSelector([
-                "HouseStyle","Foundation","KitchenQual","ExterQual","ExterCond",'Exterior1st',"LotShape"])),
-            ("encoder",  CategoryEncoder())
+            ("selector", DataFrameSelector(categorical_columns)),
+            ("encoder", CategoryEncoder())
         ])
 
+        matrix = categorical_pipeline.fit_transform(test)
 
-        print(np.unique(test['HouseStyle']))
-        print(np.unique(test['Foundation']))
-        print(np.unique(test['KitchenQual']))
-        print(np.unique(test['ExterQual']))
-        #categorical_pipeline.fit(train)
-
-        #print(categorical_pipeline.transform(test))
-        print("RRR")
-        print(test['ExterQual'])
+        assert matrix.shape == (len(test), unique_values)
