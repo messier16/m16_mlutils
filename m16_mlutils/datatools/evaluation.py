044ffa3ae774d9e6466a9525846dbb5916c43bcd
Antonio Feregrino <antonio.feregrino@gmail.com>
2019-02-17 19:46:43 +0000

    Added a few docstrings.

--- m16_mlutils/datatools/evaluation.py
+++ m16_mlutils/datatools/evaluation.py
@@ -6,6 +6,13 @@ from sklearn.metrics import (
 
 
 def get_metrics(true_labels, predicted_labels, avg='macro'):
+    """
+    Evaluate all the metrics
+    :param true_labels:
+    :param predicted_labels:
+    :param avg:
+    :return:
+    """
     precision = precision_score(true_labels, predicted_labels, average=avg)
     recall = recall_score(true_labels, predicted_labels, average=avg)
     f1 = fbeta_score(true_labels, predicted_labels, 1, average=avg)
@@ -17,9 +24,18 @@ def get_metrics(true_labels, predicted_labels, avg='macro'):
     return metrics
 
 
-def eval_summary(true_labels, predicted_labels, avg='macro', need_matrix=False):
+def eval_summary(true_labels, predicted_labels, avg='macro', need_matrix=False, summary_as_dict=False):
+    """
+    Obtain a full classification report, including metrics, a classification report, and optionally, a confusion matrix
+    :param true_labels:
+    :param predicted_labels:
+    :param avg:
+    :param need_matrix: If you need the confusion matrix, set this parameter to true.
+    :param summary_as_dict: If you want the classification report as a dict, set this parameter to true.
+    :return:
+    """
     summary = get_metrics(true_labels, predicted_labels, avg=avg)
-    report = classification_report(true_labels, predicted_labels)
+    report = classification_report(true_labels, predicted_labels, output_dict=summary_as_dict)
     if need_matrix:
         matrix = confusion_matrix(true_labels, predicted_labels)
     else:
--- m16_mlutils/plot/utils.py
+++ m16_mlutils/plot/utils.py
@@ -1,4 +1,10 @@
 def get_rows_columns(items, columns):
+    """
+    A helper to use subplots. Pass the number of items and the number of columns that you want to use to display them.
+    :param items:
+    :param columns:
+    :return: a tuple with the number of rows and columns necessary to present all your data.
+    """
     rows = items // columns
     if items < columns:
         columns = items
