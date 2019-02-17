9ac7955e99ccc1508a1b312c85aa6ed19592fadf
Antonio Feregrino <antonio.feregrino@gmail.com>
2019-02-17 19:51:57 +0000

    Update test_evaluation.py

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,13 +1,25 @@
 from m16_mlutils.datatools.evaluation import eval_summary
 
 
+def test_eval_summary_matrix_expected():
+    y_true = [True, True, True, False]
+    y_pred = [True, True, True, False]
+
+    summary, report, matrix = eval_summary(y_true, y_pred, need_matrix=True)
+
+    assert matrix.shape == (2, 2)
+    assert report
+    assert len(summary) == 4
+    assert summary['precision'] == 1
+
+
 def test_eval_summary():
     y_true = [True, True, True, False]
     y_pred = [True, True, True, False]
 
-    summary, report, matrix = eval_summary(y_true, y_pred)
+    summary, report, matrix = eval_summary(y_true, y_pred, need_matrix=False)
 
-    assert matrix.shape == (2, 2)
+    assert matrix is None
     assert report
     assert len(summary) == 4
     assert summary['precision'] == 1
